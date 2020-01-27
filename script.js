4d418a2232b12381e012e2a12edf970e8ccf9f68
emu <bsse0927@iit.du.ac.bd>
2020-01-27 14:50:03 +0600

    upto map

--- script.js
+++ script.js
@@ -279,3 +279,25 @@ function SmithPerson(firstName, yearOfBirth, lastName='Smith',nationality='Ameri
 var john = new SmithPerson('John',1990);
 
 var emily = new SmithPerson('Emily',1983, 'Diaz', 'spanish');
+
+//maps
+
+const question = new Map();
+question.set('question', 'What is the official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer :D');
+question.set(false, 'Wrong, please try again!');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+
+if(question.has(4)) {
+    question.delete(4);
+    console.log('Answer 4 is here')
+}
+//question.clear();
