538dc7be5780082f3d8920c2aeac9ac20cd5fdba
emu <bsse0927@iit.du.ac.bd>
2020-01-27 15:41:39 +0600

    upto subclass

--- script.js
+++ script.js
@@ -317,6 +317,7 @@ console.log(question.get(ans===question.get('correct')));
 */
 
 //class
+/*
 class Person6 {
     constructor (name, yearOfBirth, job) {
         this.name = name;
@@ -337,3 +338,34 @@ class Person6 {
 const john6 = new Person6('John', 1990, 'teacher');
 
 Person6.greeting();
+*/
+//subclass
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
+
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
